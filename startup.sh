199f40a103d9f5ebfb405bff5865274180bbaef9
bakito <github@bakito.ch>
2019-11-26 22:56:56 +0100

    support extensions

--- startup.sh
+++ startup.sh
@@ -4,14 +4,23 @@ INSTALL=/home/coder-install
 
 if [[ ! -f /home/coder/.setup-done ]];then
   echo "Preparing initial env ..."
-  mkdir /home/coder/h
   cp -Rf ${INSTALL}/.bashrc ${HOME}
   cp -Rf ${INSTALL}/.cache ${HOME}
   cp -Rf ${INSTALL}/.gimme ${HOME}
   cp -Rf ${INSTALL}/.local ${HOME}
   cp -Rf ${INSTALL}/workspace ${HOME}
   echo "... done"
-
+  touch /home/coder/.setup-done
 fi
 
-code-server home/coder/h/workspace
+if [[ -d /home/coder-install/extensions ]]; then
+  for ex in $(find /home/coder-install/extensions -type d); do # Not recommended, will break on whitespace
+    if [[ -f /home/coder-install/extensions/${ex}/setup.sh ]]; then
+      echo "Perparing extension ${ex} ..."
+      bash /home/coder-install/extensions/${ex}/setup.sh
+      echo "... done"
+    fi
+  done
+fi
+
+code-server ${HOME}/workspace
